ec4b144b7e5818bcaaab3f75a896ef4d43e094db
Victor Wesley <victor.infowb@gmail.com>
2025-09-23 20:51:40 -0300

    feat: Implementa entidades club,player,stadium

---
+++ src/main/java/com/java10x/elifoot/entity/Club.java
@@ -0,0 +1,36 @@
+package com.java10x.elifoot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "club_seq")
+    @SequenceGenerator(name = "club_seq", sequenceName = "club_sequence", allocationSize = 1)
+    private Long id;
+    private String name;
+    private LocalDate founded;
+    private String urlImg;
+
+    @OneToOne
+    @JoinColumn(name = "stadium_id")
+    private Stadium stadium;
+
+    @OneToMany
+    private List<Player> players;
+
+
+
+
+
+}
---
+++ src/main/java/com/java10x/elifoot/entity/Stadium.java
@@ -0,0 +1,25 @@
+package com.java10x.elifoot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Stadium {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "stadium_seq")
+    @SequenceGenerator(name = "stadium_seq", sequenceName = "stadium_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private String city;
+    private Integer capacity;
+    private String urlImage;
+
+
+
+}
