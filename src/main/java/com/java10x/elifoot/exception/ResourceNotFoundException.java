142a80828a50a0f6d969936bab82d6e6f54b78e6
Victor Wesley <victor.infowb@gmail.com>
2025-09-26 21:03:39 -0300

    feat: Adiciona ClubDetail

---
+++ src/main/java/com/java10x/elifoot/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java10x.elifoot.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
