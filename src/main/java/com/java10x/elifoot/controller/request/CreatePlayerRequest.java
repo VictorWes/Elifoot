48c5d560b7813449f08c94dc9cf437faa2ce26ca
Victor Wesley <victor.infowb@gmail.com>
2025-09-28 20:13:01 -0300

    fix: Erro lombok para getters e setters, adicionado getters e setters de forma manual

---
+++ src/main/java/com/java10x/elifoot/controller/request/CreatePlayerRequest.java
@@ -0,0 +1,4 @@
+package com.java10x.elifoot.controller.request;
+
+public class CreatePlayerRequest {
+}
--- src/main/java/com/java10x/elifoot/entity/Stadium.java
+++ src/main/java/com/java10x/elifoot/entity/Stadium.java
@@ -3,8 +3,7 @@ package com.java10x.elifoot.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,5 +20,43 @@ public class Stadium {
     private String urlImg;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
 }
