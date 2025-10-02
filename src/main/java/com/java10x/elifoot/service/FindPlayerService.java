ce57e2ae44d4c447e0ae0f3c7519e98b85bc99ae
Victor Wesley <victor.infowb@gmail.com>
2025-10-02 20:49:16 -0300

    feat: Adiciona logica para FindAll,FindId, playerservice

--- src/main/java/com/java10x/elifoot/service/FindPlayerService.java
+++ src/main/java/com/java10x/elifoot/service/FindPlayerService.java
@@ -1,4 +1,43 @@
 package com.java10x.elifoot.service;
 
+
+import com.java10x.elifoot.controller.response.PlayerDetailResponse;
+import com.java10x.elifoot.controller.response.PlayerResponse;
+import com.java10x.elifoot.exception.ResourceNotFoundException;
+import com.java10x.elifoot.mapper.PlayerMapper;
+import com.java10x.elifoot.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class FindPlayerService {
+
+    private PlayerRepository playerRepository;
+    private final PlayerMapper playerMapper;
+
+
+    public Page<PlayerResponse> findAll(Pageable pageable){
+        return playerRepository.findAll(pageable)
+                .map(playerMapper::toPlayerResponse);
+    }
+
+    public PlayerDetailResponse findById(Long id) {
+        return playerRepository.findById(id)
+                .map(playerMapper::toPlayerDetailResponse)
+                .orElseThrow(() -> new ResourceNotFoundException("Player not found with id: " + id));
+    }
+
+    public List<PlayerResponse> findByClubId(Long clubId) {
+        return playerRepository.findById(clubId)
+                .stream()
+                .map(playerMapper::toPlayerResponse)
+                .toList();
+    }
+
+
 }
