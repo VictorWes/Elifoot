1b815d00479335afa9b6f09de858bf22f116114d
Victor Wesley <victor.infowb@gmail.com>
2025-10-02 20:48:18 -0300

    feat: Service de login

--- src/main/java/com/java10x/elifoot/service/LoginService.java
+++ src/main/java/com/java10x/elifoot/service/LoginService.java
@@ -1,4 +1,65 @@
 package com.java10x.elifoot.service;
 
+import com.java10x.elifoot.controller.request.LoginRequest;
+import com.java10x.elifoot.controller.response.LoginResponse;
+import com.java10x.elifoot.entity.Scope;
+import com.java10x.elifoot.entity.User;
+import com.java10x.elifoot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
+
+
+    public LoginResponse login(LoginRequest request){
+        Optional<User> optUser = userRepository.findByEmail(request.getEmail());
+
+        if (optUser.isEmpty() || !isPasswordCorrect(request.getPassword(), optUser.get().getPassword())) {
+            throw new BadCredentialsException("Invalid email or password");
+        }
+
+        User savedUser = optUser.get();
+        List<String> scopes = savedUser.getScopes()
+                .stream()
+                .map(Scope::getName)
+                .toList();
+        long expiresIn = 600L;
+
+        JwtClaimsSet jwt = JwtClaimsSet.builder()
+                .issuer("elifoot-api")
+                .subject(savedUser.getName())
+                .expiresAt(Instant.now().plusSeconds(expiresIn))
+                .issuedAt(Instant.now())
+                .claim("email", savedUser.getEmail())
+                .claim("scope", scopes)
+                .build();
+
+        String token = jwtEncoder.encode(JwtEncoderParameters.from(jwt)).getTokenValue();
+
+        return LoginResponse.builder()
+                .acessToken(token)
+                .expiresIn(expiresIn)
+                .build();
+    }
+
+    private boolean isPasswordCorrect(String password, String savedPassword) {
+        return passwordEncoder.matches(password, savedPassword);
+    }
+
+
 }
