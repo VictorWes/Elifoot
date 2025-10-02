389243ab5e1b88f8e2543f55595cc47b8c830831
Victor Wesley <victor.infowb@gmail.com>
2025-10-02 20:49:53 -0300

    feat: Adiciona anotations personalizadas para scopos

--- src/main/java/com/java10x/elifoot/config/security/annotations/CanWritePlayer.java
+++ src/main/java/com/java10x/elifoot/config/security/annotations/CanWritePlayer.java
@@ -1,4 +1,15 @@
 package com.java10x.elifoot.config.security.annotations;
 
-public interface CanWritePlayer {
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('SCOPE_ADMIN:all, 'SCOPE_player:write')")
+public @interface CanWritePlayer {
+
 }
