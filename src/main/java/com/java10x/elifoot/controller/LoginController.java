8cfd92164c3990377cac4cd57c23f7de512a48df
Victor Wesley <victor.infowb@gmail.com>
2025-10-06 22:54:30 -0300

    feat:Controller de login

--- src/main/java/com/java10x/elifoot/controller/LoginController.java
+++ src/main/java/com/java10x/elifoot/controller/LoginController.java
@@ -1,4 +1,26 @@
 package com.java10x.elifoot.controller;
 
+import com.java10x.elifoot.controller.request.LoginRequest;
+import com.java10x.elifoot.controller.response.LoginResponse;
+import com.java10x.elifoot.service.LoginService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/login")
+@RestController
+@RequiredArgsConstructor
 public class LoginController {
+
+    private final LoginService loginService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse login(@Valid @RequestBody LoginRequest request){
+        return loginService.login(request);
+    }
+
+
 }
